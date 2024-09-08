da2db2a3939a402b5f093c7870b3bc640b3b5683
JoshuaHenriques <joshuahenriques95@gmail.com>
2024-09-08 17:30:03 -0400

    minor

--- frontend/src/worker/worker.js
+++ frontend/src/worker/worker.js
@@ -4,19 +4,16 @@ importScripts("/wasm/wasm_exec.js")
 const WASM_URL = "/wasm/main.wasm"
 
 onmessage = async (e) => {
-  console.log(`Message received from main script: ${e.data}`)
-
   const go = new Go()
-  var wasm
 
   // polyfill for older browsers
   if ('instantiateStreaming' in WebAssembly) {
     WebAssembly.instantiateStreaming(fetch(WASM_URL), go.importObject).then(function (obj) {
-      wasm = obj.instance;
       let oldLog = console.log;
       console.log = (line) => { postMessage({ message: line }) }
       go.argv = ["cixac", "-e", e.data]
-      go.run(wasm).then((_) => {
+      go.run(obj.instance).then((_) => {
+        console.log("")
         console.log = oldLog
         postMessage({
           done: true
@@ -26,11 +23,11 @@ onmessage = async (e) => {
   } else {
     fetch(WASM_URL).then(resp => resp.arrayBuffer()).then(bytes =>
       WebAssembly.instantiate(bytes, go.importObject).then(function (obj) {
-        wasm = obj.instance;
         let oldLog = console.log;
         console.log = (line) => { postMessage({ message: line }) }
         go.argv = ["cixac", "-e", e.data]
-        go.run(wasm).then((_) => {
+        go.run(obj.instance).then((_) => {
+          console.log("")
           console.log = oldLog
           postMessage({
             done: true
