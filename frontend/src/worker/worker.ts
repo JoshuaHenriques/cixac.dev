c59a692c50239eef4b0c5a32382ea3bdf6ae7030
JoshuaHenriques <joshuahenriques95@gmail.com>
2024-09-25 18:41:21 -0400

    update: better error checking and readability

--- frontend/src/worker/worker.ts
+++ frontend/src/worker/worker.ts
@@ -1,41 +1,35 @@
 console.log("Worker spawned.")
 
-// declare const importScripts: (path: string) => void
-
 importScripts("/wasm/wasm_exec.js")
 const WASM_URL = "/wasm/main.wasm"
 
 onmessage = async (e) => {
-  const go = new Go()
+  const go = new Go();
+  const input = e.data
+  let result;
 
-  // polyfill for older browsers
-  if ('instantiateStreaming' in WebAssembly) {
-    WebAssembly.instantiateStreaming(fetch(WASM_URL), go.importObject).then(function (obj) {
-      let oldLog = console.log;
-      console.log = (line) => { postMessage({ message: line }) }
-      go.argv = ["cixac", "-e", e.data]
-      go.run(obj.instance).then((_: any) => {
-        console.log("")
-        console.log = oldLog
-        postMessage({
-          done: true
-        })
-      })
-    })
-  } else {
-    fetch(WASM_URL).then(resp => resp.arrayBuffer()).then(bytes =>
-      WebAssembly.instantiate(bytes, go.importObject).then(function (obj) {
-        let oldLog = console.log;
-        console.log = (line) => { postMessage({ message: line }) }
-        go.argv = ["cixac", "-e", e.data]
-        go.run(obj.instance).then((_: any) => {
-          console.log("")
-          console.log = oldLog
-          postMessage({
-            done: true
-          })
-        })
-      })
-    )
+  try {
+    if ('instantiateStreaming' in WebAssembly) {
+      result = await WebAssembly.instantiateStreaming(fetch(WASM_URL), go.importObject);
+    } else {
+      const response = await fetch(WASM_URL);
+      const bytes = await response.arrayBuffer();
+      result = await WebAssembly.instantiate(bytes, go.importObject);
+    }
+
+    const oldLog = console.log;
+    console.log = (line) => { postMessage({ message: line }); };
+
+    try {
+      go.argv = ["cixac", "-e", input];
+      await go.run(result.instance);
+      console.log("");
+    } finally {
+      console.log = oldLog;
+    }
+  } catch (error) {
+    console.error("Error running WebAssembly:", error);
   }
-}
+
+  postMessage({ done: true });
+};
