77c3cc05f8662bd9e43fb58c1255de632a45294b
JoshuaHenriques <joshuahenriques95@gmail.com>
2025-04-05 00:27:48 -0400

    javascript obfuscator

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    obfuscatorPlugin({
+      options: {
+        debugProtection: true,
+      },
+    }),
+  ],
+});
